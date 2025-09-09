d31763ea000a92ee65b5ded98732ced4d5d977b6
Thiago Lima <thiagolima.age@gmail.com>
2025-09-08 22:41:23 -0300

    feat/ api

--- Aula 6 - APIs, Hooks e Deploy/react-dashboard/src/pages/Usuarios.jsx
+++ Aula 6 - APIs, Hooks e Deploy/react-dashboard/src/pages/Usuarios.jsx
@@ -1,8 +1,30 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
 export default function Usuarios() {
+
+  const [users, setUsers] = useState([])
+
+  useEffect(()=>{
+  fetch("https://jsonplaceholder.typicode.com/users")
+  .then(response => response.json())
+  .then(data => setUsers(data))
+  .catch(error => console.log(error))
+  .finally(console.log("FOI"))
+  },[])
+
   return (
     <div className="p-6 flex-1 bg-gray-100">
       <h1 className="text-2xl font-bold">Usuários</h1>
-      <p className="mt-2 text-gray-600">Página de usuários.</p>
+      {users.map(user => (
+        <div key={user.id}>
+          <p>Nome: {user.name}</p>
+          <p>Email: {user.email}</p>
+          <p>Rua: {user.address.street}</p>
+          <p>Geo: {user.address.geo.lat}</p>
+          <br />
+        </div>
+      ))}
     </div>
   );
 }
